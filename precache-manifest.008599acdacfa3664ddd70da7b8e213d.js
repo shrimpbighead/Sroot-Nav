a4d849757540341445325becf79cac150c628a1a
sroot <1153475545@qq.com>
2020-02-23 10:09:50 +0800

    优化资源

---
+++ precache-manifest.008599acdacfa3664ddd70da7b8e213d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81e3ed528141107212f5043c12793ed",
+    "url": "/Sroot-Nav/index.html"
+  },
+  {
+    "revision": "1fd5830b7064a4edb879",
+    "url": "/Sroot-Nav/static/css/main.336b3edb.chunk.css"
+  },
+  {
+    "revision": "c599748448b4d0c0f3c4",
+    "url": "/Sroot-Nav/static/js/2.0f7f5b0b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Sroot-Nav/static/js/2.0f7f5b0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1fd5830b7064a4edb879",
+    "url": "/Sroot-Nav/static/js/main.a980ec06.chunk.js"
+  },
+  {
+    "revision": "6b2a572de70254d0a7ff",
+    "url": "/Sroot-Nav/static/js/runtime-main.6896f105.js"
+  },
+  {
+    "revision": "8c40640e65a64f3c7593a80ec56c7f31",
+    "url": "/Sroot-Nav/static/media/blogger.8c40640e.svg"
+  },
+  {
+    "revision": "2298261fe887af6d6893824b2da61f95",
+    "url": "/Sroot-Nav/static/media/i.2298261f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Sroot-Nav/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "36642eac414e6a5b52114eff2b93d70d",
+    "url": "/Sroot-Nav/static/media/mail.36642eac.svg"
+  },
+  {
+    "revision": "379d7c494e0a91606359b63f853a98a0",
+    "url": "/Sroot-Nav/static/media/qrcode-alipay.379d7c49.png"
+  },
+  {
+    "revision": "c3c016674caf75d53f171efb7742624c",
+    "url": "/Sroot-Nav/static/media/qrcode-wechat.c3c01667.png"
+  },
+  {
+    "revision": "3edf9b94349fb754383480e66c4e3f94",
+    "url": "/Sroot-Nav/static/media/weibo.3edf9b94.svg"
+  }
+]);
